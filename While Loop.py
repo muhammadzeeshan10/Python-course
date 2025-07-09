568522cc3f1c1ca6f33a5035397ca4495ba57d95
muhammadzeeshan10 <mz1253044@gmail.com>
2025-07-09 00:38:47 -0700

    Using For Loop in Python

--- While Loop.py
+++
@@ -1,9 +0,0 @@
-print("Print 1 to 10 Number")
-#  Input
-Number=int(input("Enter Number:"))
-# Check Condition if checkin executed body 
-while Number<10:
-    Number+=1
-    print("After Sumup:",Number)
-
-# ----------------------------------------------
