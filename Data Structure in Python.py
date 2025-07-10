25485d39c47e79ce8a70ba3301e695091929ffb1
muhammadzeeshan10 <mz1253044@gmail.com>
2025-07-09 23:48:24 -0700

    Data Structure(List,Tuple,Set,Dictionary)

---
+++ Data Structure in Python.py
@@ -0,0 +1,25 @@
+
+# Create a tuple of 5 city names and print each city using a loop.
+# Tuple in Python
+
+city=('Lahore','Okara','Sialkot','Shakargarh','Narowal')
+for i in range(5):
+    print(city[i])
+
+# List in Python
+
+city=['Lahore','Okara','Sialkot','Shakargarh','Narowal']
+for i in range(5):
+    print(city[i])
+
+
+# Set in Python
+
+city={'Lahore','Okara','Sialkot','Shakargarh','Narowal'}
+for c in city:
+    print(c)
+
+# Data Dictionary 
+dic={'a':10,'b':20,'c':'Zeeshan','d':'Ali'}
+for c in dic:
+    print(dic[c])
