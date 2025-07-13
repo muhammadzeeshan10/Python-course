057367aec2ec07598a64a811ee4ddd9259835197
muhammadzeeshan10 <mz1253044@gmail.com>
2025-07-12 17:16:49 -0700

    Numpy With Basic Examples

---
+++ numpy_example.py
@@ -0,0 +1,29 @@
+# Basic Example
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+
+# Example
+print("Enter ID's from 1 to 5")
+import numpy as np
+id=np.array([101,109,110,113,114])
+print("Your ID is:",id)
+
+
+# Find the transpose of a 2D matrix.
+print("Enter Matrix which you want Transpose")
+import numpy as np
+matrix=np.array([[2,4,6],[3,6,9]])
+transposed=matrix.T
+print(matrix)
+print(transposed)
+
+
+#  Transpose of 3*3 Matrix
+print("Enter 3*3 MAtrix")
+import numpy as np
+matrix=np.array([[3,4,5],[5,6,7],[7,8,9]])
+transpose=matrix.T
+print(matrix)
+print(transpose)
